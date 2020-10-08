4b919214e4bb5e2343be0490c70033f12237e69f
Patel210 <pranavpatel2101997@gmail.com>
2020-10-08 22:40:39 +0530

    Added: Hello World program on main branch

---
+++ src/com/capgemini/datastructure/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.capgemini.datastructure;
+
+/**
+ * @author Pranav
+ * Hello World 
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
